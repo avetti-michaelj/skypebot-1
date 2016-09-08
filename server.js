676672589fa92ea46266cdabf8e3730b9c7f215d
m-r-joyce <200284329@student.georgianc.on.ca>
2016-09-08 14:41:44 -0400

    Alteration

--- server.js
+++ server.js
@@ -1,51 +1,25 @@
-var express    = require('express');        // call express
-var app        = express();                 // define our app using express
-var bodyParser = require('body-parser');
-var builder    = require('botbuilder');
+var builder = require('botbuilder');
 
-var connector  = new builder.ConsoleConnector().listen();
+var connector = new builder.ConsoleConnector().listen();
 var bot = new builder.UniversalBot(connector);
-
-// configure app to use bodyParser()
-// this will let us get the data from a POST
-app.use(bodyParser.urlencoded({ 
-    extended: true })
-);
-app.use(bodyParser.json());
-
-var port = process.env.PORT || 8080;        // set our port
-
-// ROUTES FOR OUR API
-// =============================================================================
-var router = express.Router();              // get an instance of the express Router
-
-// middleware to use for all requests
-router.use(function (req, res, next) {
-    // do logging
-    console.log('Something is happening.');
-    next(); // make sure we go to the next routes and don't stop here
+bot.dialog('/', function (session) {
+    session.send("Tell me about it...");
 });
 
-// test route to make sure everything is working (accessed at GET http://localhost:8080/api)
-router.get('/', function (req, res) {
-    res.json({ message: 'hooray! welcome to our api!' });   
+// Install logging middleware
+bot.use({
+    botbuilder: function (session, next) {
+        if (/^\/log on/i.test(session.message.text)) {
+            session.userData.isLogging = true;
+            session.send('Logging is now turned on');
+        } else if (/^\/log off/i.test(session.message.text)) {
+            session.userData.isLogging = false;
+            session.send('Logging is now turned off');
+        } else {
+            if (session.userData.isLogging) {
+                console.log('Message Received: ', session.message.text);
+            }
+            next();
+        }
+    }
 });
-
-// more routes for our API will happen here
-
-// REGISTER OUR ROUTES -------------------------------
-// all of our routes will be prefixed with /api
-router.route('/api')
-    .post(function (req, res) {
-        var content = req.body.content;
-        res.end('Confirmed');
-    })
-    .get(function (req, res) {
-
-    })
-
-// START THE SERVER
-// =============================================================================
-app.listen(port);
-console.log('Magic happens on port ' + port);
-
