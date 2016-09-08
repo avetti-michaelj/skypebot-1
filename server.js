e7515c1b12bf4a763ce2d474d72b155de9fda28f
m-r-joyce <200284329@student.georgianc.on.ca>
2016-09-08 14:37:57 -0400

    Refactor

--- server.js
+++ server.js
@@ -8,7 +8,9 @@ var bot = new builder.UniversalBot(connector);
 
 // configure app to use bodyParser()
 // this will let us get the data from a POST
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.urlencoded({ 
+    extended: true })
+);
 app.use(bodyParser.json());
 
 var port = process.env.PORT || 8080;        // set our port
@@ -18,52 +20,29 @@ var port = process.env.PORT || 8080;        // set our port
 var router = express.Router();              // get an instance of the express Router
 
 // middleware to use for all requests
-router.use(function(req, res, next) {
+router.use(function (req, res, next) {
     // do logging
     console.log('Something is happening.');
     next(); // make sure we go to the next routes and don't stop here
 });
 
 // test route to make sure everything is working (accessed at GET http://localhost:8080/api)
-router.get('/', function(req, res) {
+router.get('/', function (req, res) {
     res.json({ message: 'hooray! welcome to our api!' });   
 });
 
-router.post('/', function(req, res) {
-    bot.dialog('/', function (session) {
-        session.send("%s, I heard: %s", session.userData.name, session.message.text);
-        session.send("Say something else...");
-    });
-    res.json({ message: 'POST successful', user: session.userData.name});
-});
-/*
-bot.dialog('/', function (session) {
-    session.send("Tell me about it...");
-});
-
-// Install logging middleware
-bot.use({
-    botbuilder: function (session, next) {
-        if (/^\/log on/i.test(session.message.text)) {
-            session.userData.isLogging = true;
-            session.send('Logging is now turned on');
-        } else if (/^\/log off/i.test(session.message.text)) {
-            session.userData.isLogging = false;
-            session.send('Logging is now turned off');
-        } else {
-            if (session.userData.isLogging) {
-                console.log('Message Received: ', session.message.text);
-            }
-            next();
-        }
-    }
-});
-*/
 // more routes for our API will happen here
 
 // REGISTER OUR ROUTES -------------------------------
 // all of our routes will be prefixed with /api
-app.use('/api', router);
+router.route('/api')
+    .post(function (req, res) {
+        var content = req.body.content;
+        res.end('Confirmed');
+    })
+    .get(function (req, res) {
+
+    })
 
 // START THE SERVER
 // =============================================================================
